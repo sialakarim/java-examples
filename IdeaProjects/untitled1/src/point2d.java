531491ca584c273ce3a65ddcc72b234f704e7d49
Zied KALLEL <kallelzied@live.com>
2018-03-22 15:54:47 +0100

    Update point2d.java
    Change the code to be clear and add toString

--- IdeaProjects/untitled1/src/point2d.java
+++ IdeaProjects/untitled1/src/point2d.java
@@ -1,24 +1,26 @@
 public class point2d {
-    double x,y;
-    public point2d()
-    {
-        x=0;y=0;
-    }
-    public point2d(double a,double b)
-    {
-        x=a;
-        y=b;
 
-    }
-    public void traslation(double dx,double dy)
-    {
+    double x, y;
 
-        x+=dx;
-        y+=dy;
-    }
-    public void affiche()
-    {
-        System.out.println("point dabscisse " +x +" et ordonnee" +y);
+    public point2d() {
+        this(0, 0);
     }
 
+    public point2d(double a, double b) {
+        this.x = a;
+        this.y = b;
+    }
+
+    public void traslation(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public void affiche() {
+        System.out.println("point dabscisse " + x + " et ordonnee" + y);
+    }
+    
+    public String toString() {
+        return "point d'abscisse " + x + " et ordonnee" + y;
+    }
 }
